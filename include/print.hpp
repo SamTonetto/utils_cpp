85c07178fa65e82a79791d4f48e38b6a6813fd7b
samtonetto <samtonetto@gmail.com>
2023-06-20 17:25:55 +1000

    added queue, stack, and pq to print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -11,19 +11,25 @@
 
 #include <iostream>
 #include <map>
+#include <queue>
 #include <set>
+#include <stack>
 #include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
+// ========= Forward Declarations ===========
+
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &container);
+
 template <typename T1, typename T2>
 std::ostream &operator<<(std::ostream &os, const std::pair<T1, T2> &container);
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::set<T> &container);
+
 template <typename T>
 std::ostream &operator<<(std::ostream &os,
                          const std::unordered_set<T> &container);
@@ -34,6 +40,18 @@ std::ostream &operator<<(std::ostream &os, const std::map<K, V> &container);
 template <typename K, typename V>
 std::ostream &operator<<(std::ostream &os,
                          const std::unordered_map<K, V> &container);
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::stack<T> &container);
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::queue<T> &container);
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os,
+                         const std::priority_queue<T> &container);
+
+// ========= Implementations ===========
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &container) {
@@ -113,5 +131,42 @@ std::ostream &operator<<(std::ostream &os,
     }
   }
   os << "}";
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::stack<T> &container) {
+  std::stack<T> temp = container;
+  std::vector<T> vec;
+  while (!temp.empty()) {
+    vec.push_back(temp.top());
+    temp.pop();
+  }
+  os << vec;
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::queue<T> &container) {
+  std::queue<T> temp = container;
+  std::vector<T> vec;
+  while (!temp.empty()) {
+    vec.push_back(temp.front());
+    temp.pop();
+  }
+  os << vec;
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os,
+                         const std::priority_queue<T> &container) {
+  std::priority_queue<T> temp = container;
+  std::vector<T> vec;
+  while (!temp.empty()) {
+    vec.push_back(temp.top());
+    temp.pop();
+  }
+  os << vec;
   return os;
 }
--- tests/test_print.cpp
+++ tests/test_print.cpp
@@ -6,34 +6,29 @@
 #include <sstream>
 
 TEST_CASE("print") {
-  std::vector<int> v{1, 2, 3, 4, 5};
-  std::set<int> s{1, 2, 3, 4, 5};
-  std::map<int, int> m{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}};
-  std::unordered_map<int, int> um{{1, 1}, {2, 2}};
-  std::unordered_set<int> us{1, 2};
-
-  std::vector<std::map<int, int>> v_m{{{1, 1}}, {{2, 2}}};
+  std::stringstream ss;
 
   SUBCASE("vector") {
-    std::stringstream ss;
+    std::vector<int> v{1, 2, 3, 4, 5};
+
     ss << v;
     CHECK(ss.str() == "[1, 2, 3, 4, 5]");
   }
 
   SUBCASE("set") {
-    std::stringstream ss;
+    std::set<int> s{1, 2, 3, 4, 5};
     ss << s;
     CHECK(ss.str() == "{1, 2, 3, 4, 5}");
   }
 
   SUBCASE("map") {
-    std::stringstream ss;
+    std::map<int, int> m{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}};
     ss << m;
     CHECK(ss.str() == "{1: 1, 2: 2, 3: 3, 4: 4, 5: 5}");
   }
 
   SUBCASE("unordered_map") {
-    std::stringstream ss;
+    std::unordered_map<int, int> um{{1, 1}, {2, 2}};
     ss << um;
 
     bool is_correct = ss.str() == "{1: 1, 2: 2}" || ss.str() == "{2: 2, 1: 1}";
@@ -41,7 +36,7 @@ TEST_CASE("print") {
   }
 
   SUBCASE("unordered_set") {
-    std::stringstream ss;
+    std::unordered_set<int> us{1, 2};
     ss << us;
 
     bool is_correct = ss.str() == "{1, 2}" || ss.str() == "{2, 1}";
@@ -49,9 +44,39 @@ TEST_CASE("print") {
   }
 
   SUBCASE("vector of map") {
-    std::stringstream ss;
+    std::vector<std::map<int, int>> v_m{{{1, 1}}, {{2, 2}}};
     ss << v_m;
 
     CHECK(ss.str() == "[{1: 1}, {2: 2}]");
+  }
+
+  SUBCASE("queue") {
+    std::queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    ss << q;
+
+    CHECK(ss.str() == "[1, 2, 3]");
+  }
+
+  SUBCASE("stack") {
+    std::stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    ss << s;
+
+    CHECK(ss.str() == "[3, 2, 1]");
+  }
+
+  SUBCASE("priority_queue") {
+    std::priority_queue<int> pq;
+    pq.push(1);
+    pq.push(2);
+    pq.push(3);
+    ss << pq;
+
+    CHECK(ss.str() == "[3, 2, 1]");
   }
 }
