ea5de2ffa26f18bd0176402ad77ea6191e37866d
samtonetto <samtonetto@gmail.com>
2023-07-06 16:18:02 +1000

    added timing decorator

---
+++ include/timing.hpp
@@ -0,0 +1,76 @@
+/**********************************************************************
+ * @brief Timing utilities
+ * @details
+ * @author Sam Tonetto
+ * @copyright GNU Public License
+ * @date 2023
+ ***********************************************************************/
+
+#pragma once
+
+#include <chrono>
+#include <future>
+#include <optional>
+#include <utility>
+
+namespace utils {
+
+/**
+ * Timing decorator. You can wrap any function in it and it will return a pair,
+ * where the first element is an std::optional containing the result of the
+ * function (optional to handle potentially void functions) and the second
+ * element is the time it took to execute the function in nanoseconds.
+ *
+ * Note that you have to define the wrapper function as constexpr if you want it
+ * to be compile-time.
+ *
+ * @example
+ * int increment(int x) { return x + 1; }
+ * int main() {
+ *  constexpr auto timed_increment = time(increment);
+ *  auto [result, duration] =
+ *  timed_increment(1);
+ *
+ *  std::cout << "Result: " << result.value() << std::endl;
+ *  std::cout << "Duration: " << duration << std::endl;
+ * }
+ *
+ *
+ */
+template <typename F>
+constexpr auto time(const F &func) {
+
+  return [func](auto &&...args) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    using func_return_type =
+        decltype(func(std::forward<decltype(args)>(args)...));
+
+    std::optional<typename std::conditional<std::is_void_v<func_return_type>,
+                                            int, func_return_type>::type>
+        optional_result;
+
+    if constexpr (std::is_same_v<func_return_type, void>) {
+      func(std::forward<decltype(args)>(args)...);
+      auto end = std::chrono::high_resolution_clock::now();
+
+      auto duration_ns =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+              .count();
+
+      return std::make_pair(optional_result, duration_ns);
+    } else {
+      optional_result = func(std::forward<decltype(args)>(args)...);
+
+      auto end = std::chrono::high_resolution_clock::now();
+
+      auto duration_ns =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+              .count();
+
+      return std::make_pair(optional_result, duration_ns);
+    }
+  };
+}
+
+} // namespace utils
---
+++ tests/test_timing.cpp
@@ -0,0 +1,36 @@
+#include <optional>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "timing.hpp"
+
+#include <iostream>
+
+void wait_1us() { std::this_thread::sleep_for(std::chrono::microseconds(1)); }
+int wait_1us_and_increment(int x) {
+  wait_1us();
+  return x + 1;
+}
+
+int test_increment(int x) { return x + 1; }
+
+TEST_CASE("test timing a void function") {
+
+  auto timed_function = utils::time(wait_1us);
+
+  auto [result, duration] = timed_function();
+
+  CHECK(!result.has_value());
+  CHECK(duration > 0);
+}
+
+TEST_CASE("test timing a function with return value") {
+
+  auto timed_function = utils::time(wait_1us_and_increment);
+
+  auto [result, duration] = timed_function(1);
+
+  CHECK(result.has_value());
+  CHECK(result.value() == 2);
+  CHECK(duration > 0);
+}
